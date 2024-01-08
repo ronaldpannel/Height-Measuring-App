a173e576e61b690d98b1661662995630746925ac
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2024-01-08 12:31:50 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-function main(){
-    window.addEventListener("deviceorientation",onOrientationChange)
+function main() {
+  window.addEventListener("deviceorientation", onOrientationChange);
 
-    navigator.mediaDevices.getUserMedia({video:{
-        facingMode:'environment'
-    }})
-        .then(function(signal){
-            const video=document.getElementById("myVideo");
-            video.srcObject=signal;
-            video.play();
-        })
-        .catch(function (err){
-            alert(err);
-        })
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (signal) {
+      const video = document.getElementById("myVideo");
+      video.srcOject = signal;
+      video.play();
+    })
+    .catch(function (err) {
+      alert(err);
+    });
 }
 
-function onOrientationChange(event){
-    let angle=event.beta-90;
-    if(angle<0){
-        angle=0;
-    }
+function onOrientationChange(event) {
+  let angle = event.beta - 90;
+  if (angle < 0) {
+    angle = 0;
+  }
 
-    const distToTree=document.getElementById("mySlider").value;
-    document.getElementById("myLabel").innerHTML=
-        "Distance to tree: "+distToTree+" meters";
-    const height=Math.tan(angle*Math.PI/180)*distToTree;
-    document.getElementById("heightInfo").innerHTML=
-        height.toFixed(1)+" m (" +angle.toFixed(1)+"&deg;)";
-    
+  const distanceToObject = document.getElementById("mySlider").value;
+  document.getElementById("myLabel").innerHTML =
+    "Distance to object: " + distanceToObject + " meters";
+  const height = Math.tan((angle * Math.PI) / 180) * distanceToObject;
+  document.getElementById("heightInfo").innerHTML =
+    height.toFixed(1) + " m(" + angle.toFixed(1) + " &deg;)";
+  console.log(height.toFixed(1) + " m(" + angle.toFixed(1) + " &ged;)");
 }
